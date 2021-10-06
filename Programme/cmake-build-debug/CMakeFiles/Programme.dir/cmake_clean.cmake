853eb7156d2c84c1c68b7a8fe1cab935b6b1fc7c
TBillen <tobias.pascal.billen@gmail.com>
2021-10-06 16:51:04 +0200

    First Commit

---
+++ Programme/cmake-build-debug/CMakeFiles/Programme.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programme.dir/main.cpp.obj"
+  "Programme.exe"
+  "Programme.exe.manifest"
+  "Programme.lib"
+  "Programme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Programme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programme/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int sum();
+
+int main() {
+    std::cout << "Hello, World!" << sum() << std::endl;
+    return 0;
+}
+
+int sum(){
+    return 21 +21;
+}
